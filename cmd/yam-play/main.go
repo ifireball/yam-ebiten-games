58b1448c8dd609d4580adbde74ff1ac19e8a3bd4
Barak Korren <bkorren@redhat.com>
2024-06-23 21:42:20 +0300

    resources: Embed "four_trees.svg"
    Also some throw-away test code in main().

--- cmd/yam-play/main.go
+++ cmd/yam-play/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/ifireball/yam-ebiten-games/resources"
 )
 
 const (
@@ -24,6 +26,16 @@ func (g *Game) Update(screen *ebiten.Image) error {
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Yam Play")
+
+	file, err := resources.Resources.Open("svg/four_trees.svg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf := [1024]byte{}
+	if amount, err := file.Read(buf[:]); err == nil {
+		fmt.Println(string(buf[:amount]))
+	}
+
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
---
+++ resources/embed.go
@@ -0,0 +1,6 @@
+package resources
+
+import "embed"
+
+//go:embed svg
+var Resources embed.FS
