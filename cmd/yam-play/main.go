52826291628955f669447493b23b32edea93a002
Barak Korren <bkorren@redhat.com>
2024-06-23 23:14:41 +0300

    main: POC: Draw embedded SVG with oksvg

--- cmd/yam-play/main.go
+++ cmd/yam-play/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"image"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/ifireball/yam-ebiten-games/resources"
+	"github.com/srwiley/oksvg"
+	"github.com/srwiley/rasterx"
 )
 
 const (
@@ -13,30 +15,48 @@ const (
 	screenHeight = 1080 / 2
 )
 
-type Game struct{}
+type Game struct{
+	Background *ebiten.Image
+}
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
+	if g.Background == nil {
+		file, err := resources.Resources.Open("svg/four_trees.svg")
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+	
+		imageSvg, err := oksvg.ReadIconStream(file)
+		if err != nil {
+			return err
+		}
+		
+		imageBitmap := image.NewRGBA(image.Rect(0, 0, screenWidth, screenHeight))
+		scannerGV := rasterx.NewScannerGV(screenWidth, screenHeight, imageBitmap, imageBitmap.Bounds())
+		dasher := rasterx.NewDasher(screenWidth, screenHeight, scannerGV)
+		//imageSvg.Transform = imageSvg.Transform.Scale(0.5, 0.5)
+		imageSvg.Draw(dasher, 1.0)
+		
+		g.Background, _ = ebiten.NewImageFromImage(imageBitmap, ebiten.FilterDefault)
+	}
 	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.DrawImage(g.Background, &ebiten.DrawImageOptions{})
 }
 
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Yam Play")
 
-	file, err := resources.Resources.Open("svg/four_trees.svg")
-	if err != nil {
-		log.Fatal(err)
-	}
-	buf := [1024]byte{}
-	if amount, err := file.Read(buf[:]); err == nil {
-		fmt.Println(string(buf[:amount]))
-	}
-
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	game := Game{}
+	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
 	}
 }
