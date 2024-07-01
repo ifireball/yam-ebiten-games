6e48027b71dcfb091eee0e97eadc1ed002985046
Barak Korren <bkorren@redhat.com>
2024-07-01 22:54:26 +0300

    author: Refactor mouse again
    Now all mouse handling is in the mouse object and we can differentiate
    between clicks and releasing the mouse after dragging it.

--- pkg/author/fruit.go
+++ pkg/author/fruit.go
@@ -2,7 +2,6 @@ package author
 
 import (
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/inpututil"
 	"github.com/ifireball/yam-ebiten-games/pkg/fourtrees/fruit"
 	"github.com/ifireball/yam-ebiten-games/pkg/gmath"
 )
@@ -28,34 +27,29 @@ func (f *Fruit) Update(screen *ebiten.Image) error {
 		f.initialized = true
 	}
 
-	f.mouse.UpdateLocation()
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-		if loc := f.findLocationAtMouse(); loc != nil {
-			f.mouse.StartDrag(&loc.position)
-		}
-	}
-	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
-		if f.mouse.IsDragging() {
-			f.mouse.Drop()
-		} else {
-			f.addLocationAtMouse()
-		}
-	}
+	f.mouse.Update(MouseUpdateHandlers{
+		OnDragStart: func(mouseLoc *gmath.Vec2, setDraggedLoc func(draggedLoc *gmath.Vec2, onDrop func())) {
+			if loc := f.findLocationAt(mouseLoc); loc != nil {
+				setDraggedLoc(&loc.position, nil)
+			}
+		},
+		OnClick: f.addLocationAt,
+	})
 
 	return nil
 }
 
-func (f *Fruit) addLocationAtMouse() {
+func (f *Fruit) addLocationAt(position *gmath.Vec2) {
 	var l Location
-	l.position = f.mouse.Location
+	l.position = *position
 	l.position.Sub(&gmath.Vec2{X: float64(fruit.Width)/2, Y: float64(fruit.Height)/2})
 	f.locations = append(f.locations, l)
 }
 
-func (f *Fruit) findLocationAtMouse() *Location {
+func (f *Fruit) findLocationAt(position *gmath.Vec2) *Location {
 	for i := range f.locations {
 		loc := &f.locations[i]
-		if f.mouse.IsTouchingRect(&loc.position, fruit.Width, fruit.Height) {
+		if position.InRect(&loc.position, fruit.Width, fruit.Height) {
 			return loc
 		}
 	}
@@ -63,8 +57,7 @@ func (f *Fruit) findLocationAtMouse() *Location {
 }
 
 func (f *Fruit) Draw(screen *ebiten.Image) {
-	f.mouse.UpdateLocation()
-	f.mouse.UpdateDrag()
+	f.mouse.Draw()
 	dio := ebiten.DrawImageOptions{}
 	for i := 0; i < len(f.locations); i++ {
 		dio.GeoM.Reset()
--- pkg/author/mouse.go
+++ pkg/author/mouse.go
@@ -1,42 +1,71 @@
 package author
 
 import (
+	"math"
+
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/inpututil"
 	"github.com/ifireball/yam-ebiten-games/pkg/gmath"
 )
 
 type Mouse struct {
-	Location gmath.Vec2
+	pressLocation gmath.Vec2
+	dragStarted bool
 	draggedLoc *gmath.Vec2
 	draggedOffset gmath.Vec2
+	dropFunc func()
 }
 
-func (m *Mouse) UpdateLocation() {
-	m.Location.SetInts(ebiten.CursorPosition())
+type MouseUpdateHandlers struct {
+	OnClick func(mouseLoc *gmath.Vec2)
+	OnDragStart func(
+		mouseLoc *gmath.Vec2, 
+		setDraggedLoc func(draggedLoc *gmath.Vec2, onDrop func()),
+	)
 }
 
-func (m *Mouse) StartDrag(draggedLoc *gmath.Vec2) {
-	m.draggedLoc = draggedLoc
-	m.draggedOffset = *draggedLoc
-	m.draggedOffset.Sub(&m.Location)
+func (m *Mouse) Update(handlers MouseUpdateHandlers) {	
+	var location gmath.Vec2
+	location.SetInts(ebiten.CursorPosition())
+
+	switch {
+	case inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft):
+		m.pressLocation = location
+	case ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft): 
+		if !m.dragStarted && m.pressDistance(location) > 1 {
+			m.dragStarted = true
+			if handlers.OnDragStart != nil {
+				handlers.OnDragStart(&m.pressLocation, func(draggedLoc *gmath.Vec2, onDrop func()) {
+					m.draggedLoc = draggedLoc
+					m.draggedOffset = *draggedLoc
+					m.draggedOffset.Sub(&location)
+					m.dropFunc = onDrop
+				})
+			}
+		}
+	case inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft):
+		if m.dragStarted {
+			m.dragStarted = false
+			m.draggedLoc = nil
+			if m.dropFunc != nil {
+				m.dropFunc()
+			}
+		} else if handlers.OnClick != nil {
+			handlers.OnClick(&location)
+		}
+	} 
 }
 
-func (m *Mouse) Drop() {
-	m.draggedLoc = nil
+func (m *Mouse) pressDistance(location gmath.Vec2) float64 {
+	return max(
+		math.Abs(location.X - m.pressLocation.X), 
+		math.Abs(location.Y - m.pressLocation.Y),
+	)
 }
 
-func (m *Mouse) UpdateDrag() {
-	if !m.IsDragging() {
-		return
+func (m *Mouse) Draw() {
+	if m.draggedLoc != nil {
+		m.draggedLoc.SetInts(ebiten.CursorPosition())
+		m.draggedLoc.Add(&m.draggedOffset)
 	}
-	*m.draggedLoc = m.Location
-	m.draggedLoc.Add(&m.draggedOffset)
-}
-
-func (m *Mouse) IsDragging() bool {
-	return m.draggedLoc != nil
-}
-
-func (m *Mouse) IsTouchingRect(r *gmath.Vec2, rw, rh float64) bool {
-	return m.Location.InRect(r, rw, rh)
 }
