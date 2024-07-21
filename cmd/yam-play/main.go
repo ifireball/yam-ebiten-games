ea2b6bee55c977d0d67dc83ac27c59c35d7619c9
Barak Korren <bkorren@redhat.com>
2024-07-21 22:27:33 +0300

    game: Full screen, Window Resize, Exit

--- cmd/yam-play/main.go
+++ cmd/yam-play/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/ifireball/yam-ebiten-games/pkg/fourtrees"
 	"github.com/ifireball/yam-ebiten-games/pkg/gdata"
 	"github.com/ifireball/yam-ebiten-games/pkg/gmath"
 	"github.com/ifireball/yam-ebiten-games/resources"
+)
+
+var (
+	ErrExitGame = errors.New("exit game")
 )
 
 type Game struct {
@@ -30,6 +36,14 @@ func (g *Game) Update() error {
 			return err
 		}
 	}
+
+	if inpututil.IsKeyJustReleased(ebiten.KeyEscape) {
+		return ErrExitGame
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyF11) {
+		ebiten.SetFullscreen(!ebiten.IsFullscreen())
+	}
+
 	if err := g.Girl.Update(); err != nil {
 		return err
 	}
@@ -61,12 +75,13 @@ func (g *Game) Draw(screen *ebiten.Image) {
 func main() {
 	ebiten.SetWindowSize(gdata.ScreenWidth, gdata.ScreenHeight)
 	ebiten.SetWindowTitle("Yam Play")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
 	var game Game
 	if err := resources.DecodeData("fourtrees", &game); err != nil {
 		log.Fatal(err)
 	}
-	if err := ebiten.RunGame(&game); err != nil {
+	if err := ebiten.RunGame(&game); err != nil && !errors.Is(err, ErrExitGame) {
 		log.Fatal(err)
 	}
 }
