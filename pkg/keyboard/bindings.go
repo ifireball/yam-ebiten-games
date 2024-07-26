42f3179ad7e4c15334b72f266db895a07ef2d11d
Barak Korren <bkorren@redhat.com>
2024-07-26 09:33:03 +0300

    keyboard:bindings: Fix for WoBee
    Turns out I was off by one (key).

--- pkg/keyboard/bindings.go
+++ pkg/keyboard/bindings.go
@@ -5,10 +5,10 @@ import (
 )
 
 const (
-	WobeeBlue = ebiten.KeyA
-	WobeeYellow = ebiten.KeyF
-	WobeeRed = ebiten.KeyJ
-	WobeeGreen = ebiten.KeySemicolon
+	WobeeBlue = ebiten.KeyS
+	WobeeYellow = ebiten.KeyG
+	WobeeRed = ebiten.KeyK
+	WobeeGreen = ebiten.KeyApostrophe
 )
 
 var WobeeBlueGreen = NewCombo(WobeeBlue, WobeeGreen)
